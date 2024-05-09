eef12283e15dc6971377887d97038c3d5cb0f2ca
Delton71 <ulev3@yandex.ru>
2024-05-09 21:46:50 +0300

    solution first commit

--- solution.py
+++ solution.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1265544018 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array, y: np.array) -> bool:
+  alpha = 0.08
+  res = ttest_ind(x, y, equal_var=False, 
+                  alternative='greater')
+
+  return res.pvalue < alpha
